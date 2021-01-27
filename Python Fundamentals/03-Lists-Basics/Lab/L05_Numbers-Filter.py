1a6fc19f324fe2064aa12527e48d37aa207b06ba
Dimitar I. Stoyanov <55301791+mitaccino@users.noreply.github.com>
2021-01-27 16:27:41 +0100

    Add files via upload

---
+++ Python Fundamentals/03-Lists-Basics/Lab/L05_Numbers-Filter.py
@@ -0,0 +1,29 @@
+# 03. Lab: Lists Basics
+# ----------------------
+# 05. Numbers Filter
+
+n = int(input())
+numbers = []
+filtered = []
+
+for i in range(n):
+    curr_number = int(input())
+    numbers.append(curr_number)
+command = input()
+if command == "even":
+    for number in numbers:
+        if number % 2 == 0:
+            filtered.append(number)
+elif command == "odd":
+    for number in numbers:
+        if number % 2 != 0:
+            filtered.append(number)
+elif command == "negative":
+    for number in numbers:
+        if number < 0:
+            filtered.append(number)
+elif command == "positive":
+    for number in numbers:
+        if number >= 0:
+            filtered.append(number)
+print(filtered)
