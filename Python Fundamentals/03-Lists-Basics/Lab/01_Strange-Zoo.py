2bfac3519adcd91d2a022c54ae9ea5bce052776e
Dimitar I. Stoyanov <55301791+mitaccino@users.noreply.github.com>
2021-01-27 15:51:36 +0100

    Add files via upload

---
+++ Python Fundamentals/03-Lists-Basics/Lab/01_Strange-Zoo.py
@@ -0,0 +1,13 @@
+# 03. Lab: Lists Basics
+# ----------------------
+# 01. Strange Zoo
+
+tail = input()
+body = input()
+head = input()
+
+meerkat = [tail, body, head]
+
+meerkat[0], meerkat[2] = meerkat[2], meerkat[0]
+
+print(meerkat)
---
+++ Python Fundamentals/03-Lists-Basics/Lab/02_Courses.py
@@ -0,0 +1,12 @@
+# 03. Lab: Lists Basics
+# ----------------------
+# 02. Courses
+
+n = int(input())
+courses = []
+
+for i in range(n):
+    current_course = input()
+    courses.append(current_course)
+
+print(courses)
