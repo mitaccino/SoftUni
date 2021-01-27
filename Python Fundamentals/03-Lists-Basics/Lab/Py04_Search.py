e1a64aaf4aae7191f95dd748fe855f47e2a8cd9d
Dimitar I. Stoyanov <55301791+mitaccino@users.noreply.github.com>
2021-01-27 16:14:04 +0100

    Add files via upload

---
+++ Python Fundamentals/03-Lists-Basics/Lab/Py04_Search.py
@@ -0,0 +1,18 @@
+# 03. Lab: Lists Basics
+# ----------------------
+# 04. Search
+
+n = int(input())
+word = input()
+strings = []
+
+for i in range(n):
+    curr_string = input()
+    strings.append(curr_string)
+print(strings)
+
+for i in range(len(strings) -1, -1, -1):
+    element = strings[i]
+    if word not in element:
+        strings.remove(element)
+print(strings)
